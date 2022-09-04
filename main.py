90130b5687ce573ab7fd7ced792d5f892a7cfba8
spedrickson <spedrickson@gmail.com>
2022-09-03 20:13:02 -0700

    support environment variable configuration

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import os
+
 from sys import argv
 from channel import YtChannel
 
 channel_id = "UCrPseYLGpNygVi34QpGNqpA" if len(argv) == 1 else argv[1]
+channel_id = os.environ['YTCHAT_CHANNELID'] if 'YTCHAT_CHANNELID' in os.environ else channel_id
+
 channel = YtChannel(channel_id)
 if len(argv) > 2:
     channel.ingest_video(argv[2])
