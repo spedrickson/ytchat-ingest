4bbefce8e45baed8a579036eeb1de28043200d9d
spedrickson <spedrickson@gmail.com>
2023-01-15 02:02:09 -0800

    remove pywright and manual ingest sleep

--- channel.py
+++ channel.py
@@ -1,13 +1,17 @@
-import datetime
+# standard library
 import json
 import sys
 from time import sleep
-from urllib import parse
+from urllib.parse import urlparse
+from urllib.parse import parse_qs
 
+# requirements
 import pytchat
+import requests
 from loguru import logger
-from playwright.sync_api import sync_playwright, TimeoutError as PlaywrightTimeoutError
+from lxml import html
 
+# project
 import utils
 from db_connector import ChatDB
 
@@ -22,25 +26,24 @@ class YtChannel:
         if len(str(channel_id)) == 0:
             raise RuntimeError("cannot create YtChannel instance without channel ID")
 
-    def get_live_id(self, browser):
+    def get_live_id(self):
         """Queries the channel for a live or scheduled broadcast\n
         example: `yCuHrjO_LK8` from the URL: `https://www.youtube.com/watch?v=yCuHrjO_LK8`\n
         :returns: video id for the live/scheduled broadcast, or None if no video found"""
-        url = f"https://www.youtube.com/channel/{self.channel_id}/live"
-        logger.info(f"testing channel live status: {url}")
-        try:
-            page = browser.new_page()
-            page.goto(url, wait_until="networkidle")
-            logger.debug(f"page url after load: {page.url}")
-            split = parse.urlsplit(page.url)
-            page.close()
-            # logger.debug(f"url query: {split.query}")
-            query = dict(parse.parse_qs(split.query))
-            video_id = query.get("v", None)
-            return video_id[0] if video_id else None
-        except PlaywrightTimeoutError as e:
-            logger.error("timeout error while polling live status")
-            logger.error(str(e))
+        if self.channel_id.startswith('@'):
+            url = f'https://youtube.com/{self.channel_id}/live'
+        else:
+            url = f'https://youtube.com/channel/{self.channel_id}/live'
+        logger.debug(f"testing channel live status: {url}")
+        page = requests.get(url)
+        tree = html.fromstring(page.content)
+        canonical_url = str(tree.xpath('//link[@rel="canonical"]/@href')[0])
+        if "/channel/" not in canonical_url:
+            logger.info(f"found live url ({canonical_url}) for channel {self.channel_id}")
+            parsed_url = urlparse(canonical_url)
+            video_id = parse_qs(parsed_url.query)['v'][0]
+            return video_id
+        return None
 
     def ingest(self):
         logger.info(f"starting to endlessly ingest chat for channel: {self.channel_id}")
@@ -58,10 +61,7 @@ class YtChannel:
     def ingest_video(self, video_id: str):
         count = err_count = 0
         chat = pytchat.create(video_id=video_id)
-        last_ingest = datetime.datetime.now()
         while chat.is_alive():
-            utils.minimum_wait(last_ingest)
-            last_ingest = datetime.datetime.now()
             messages = chat.get().sync_items()
             json_messages = list(map(lambda m: json.loads(m.json()), messages))
             l_count = len(json_messages)
@@ -79,24 +79,17 @@ class YtChannel:
            :returns: video id for the live/scheduled broadcast"""
         logger.info(f"waiting for video_id on channel: {self.channel_id}")
         video_id = None
-        with sync_playwright() as p:
-            browser = p.chromium.launch()
-            while not video_id:
-                video_id = self.get_live_id(browser)
-                if not video_id:
-                    logger.debug(f"sleeping 90 seconds before checking status again")
-                    sleep(90)
-            browser.close()
-
+        while not video_id:
+            video_id = self.get_live_id()
+            if not video_id:
+                logger.debug(f"sleeping 90 seconds before checking status again")
+                sleep(90)
         return video_id
 
     def test(self, video_id: str):
         count = err_count = 0
         chat = pytchat.create(video_id=video_id)
-        last_ingest = datetime.datetime.now()
         while chat.is_alive():
-            utils.minimum_wait(last_ingest)
-            last_ingest = datetime.datetime.now()
             messages = chat.get().sync_items()
             json_messages = list(map(lambda m: json.loads(m.json()), messages))
             l_count = len(json_messages)
@@ -106,25 +99,9 @@ class YtChannel:
             err_count += l_err
             logger.info(f"attempted to add {l_count} items. had: {l_err} errors/duplicates")
         logger.info(f"total had {count} items. {err_count} errors/duplicates")
-        # success_count = self.conn.insert_comments(list(messages.sync_items()))
-
-        # if not messages:
-        #     logger.warning("no messages?")
-        #     continue
-        # items = messages.get('items', None)
-        # if not items:
-        #     continue
-        # json_messages = list(map(lambda m: json.loads(m.json()), messages))
-        # l_count = len(json_messages)
-        # l_err = l_count - success_count
-        # count += l_count
-        # err_count += l_err
-        # logger.info(f"attempted to add {l_count} items. had: {l_err} errors/duplicates")
-        # logger.info(f"total had {count} items. {err_count} errors/duplicates")
         chat.terminate()
 
     # pass these methods on to ChatDB connector
-
     def insert_comments(self, comments: list):
         return self.conn.insert_comments(comments)
 
